663603ad9f9fd619424161e328c2e5fde7b78859
RD-5 <radwivedi007@gmail.com>
2026-06-11 10:48:38 +0530

    Time: 5 ms (78.83%) | Memory: 46 MB (33.73%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rev=0,y=x;
+        if(x<0)
+        return false;
+        while(x!=0)
+        {
+            int digit=x%10;
+            x/=10;
+            rev=rev*10+digit;
+        }
+        return y==rev;
+    }
+}
